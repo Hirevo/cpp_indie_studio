c21796c0f390b349d4f02ddd98742b5db073b7a2
Benjamin <benjamin.solca@epitech.eu>
2018-05-09 16:31:24 +0200

    Test avec un perso et un modele

--- src/main.cpp
+++ src/main.cpp
@@ -8,73 +8,60 @@
 #include <irrlicht.h>
 
 
-int main() {
+int main(void) {
 
-	irr::IrrlichtDevice* device = irr::createDevice(  // creation du device
-		irr::video::EDT_OPENGL,                       // API = OpenGL
-		irr::core::dimension2d<irr::u32>(200,200),    // taille fenetre 640x480p
-		32);                                          // 32 bits par pixel
-
-	irr::video::IVideoDriver* driver =                // video driver
+	irr::IrrlichtDevice* device = irr::createDevice(    // creation du device
+		irr::video::EDT_OPENGL,                           // API = OpenGL
+		irr::core::dimension2d<irr::u32>(640,480),        // taille fenetre 640x480p
+		32);                                              // 32 bits par pixel
+	irr::video::IVideoDriver* driver =                  // creation du driver video
 		device->getVideoDriver ();
-	irr::scene::ISceneManager* sceneManager =         // scene manager
+	irr::scene::ISceneManager* sceneManager =           // creation du scene manager
 		device->getSceneManager ();
+	device->getCursorControl ()-> setVisible (false);   // rend le curseur invisible
 
-	device->getCursorControl ()-> setVisible (false); // curseur invisible
 
+	/* MODELE */
 
-	/* CUBE */
+	irr::scene::IAnimatedMeshSceneNode *sydney =        // cree un scene node nomme sydney
+		sceneManager->addAnimatedMeshSceneNode (          // via le scene manager
+			sceneManager->getMesh ("sydney.md2"));          // en chargeant le mesh "sydney.md2"
 
-	irr::scene::IMeshSceneNode* cube =         // pointeur vers le node
-		sceneManager->addCubeSceneNode(        // la creation du cube
-			10.0f,                             // cote de 10 unites
-			0,                                 // parent = racine
-			-1,                                // pas d'ID
-			irr::core::vector3df(              // le vecteur de position
-				0.0f,                          // origine en X
-				0.0f,                          // origine en Y
-				20.0f));                       // +20 unites en Z
+	sydney->setMaterialFlag(
+		irr::video::EMF_LIGHTING, false);
+	sydney->setMaterialTexture(
+		0, driver->getTexture("sydney.bmp"));
 
-	cube->setMaterialFlag(irr::video::EMF_WIREFRAME, true);
+	sydney->setMD2Animation(irr::scene::EMAT_STAND);    // joue l'animation STAND en boucle
 
 
 	/* CAMERA */
 
-	irr::SKeyMap keyMap[5];                    // re-assigne les commandes
-	keyMap[0].Action = irr::EKA_MOVE_FORWARD;  // avancer
-	keyMap[0].KeyCode = irr::KEY_KEY_W;        // w
-	keyMap[1].Action = irr::EKA_MOVE_BACKWARD; // reculer
-	keyMap[1].KeyCode = irr::KEY_KEY_S;        // s
-	keyMap[2].Action = irr::EKA_STRAFE_LEFT;   // a gauche
-	keyMap[2].KeyCode = irr::KEY_KEY_A;        // a
-	keyMap[3].Action = irr::EKA_STRAFE_RIGHT;  // a droite
-	keyMap[3].KeyCode = irr::KEY_KEY_D;        // d
-	keyMap[4].Action = irr::EKA_JUMP_UP;       // saut
-	keyMap[4].KeyCode = irr::KEY_SPACE;        // barre espace
+	irr::SKeyMap keyMap[5];                             // re-assigne les commandes
+	keyMap[0].Action = irr::EKA_MOVE_FORWARD;           // avancer
+	keyMap[0].KeyCode = irr::KEY_KEY_W;                 // w
+	keyMap[1].Action = irr::EKA_MOVE_BACKWARD;          // reculer
+	keyMap[1].KeyCode = irr::KEY_KEY_S;                 // s
+	keyMap[2].Action = irr::EKA_STRAFE_LEFT;            // a gauche
+	keyMap[2].KeyCode = irr::KEY_KEY_A;                 // a
+	keyMap[3].Action = irr::EKA_STRAFE_RIGHT;           // a droite
+	keyMap[3].KeyCode = irr::KEY_KEY_D;                 // d
+	keyMap[4].Action = irr::EKA_JUMP_UP;                // saut
+	keyMap[4].KeyCode = irr::KEY_SPACE;                 // barre espace
 
-	sceneManager->addCameraSceneNodeFPS(       // ajout de la camera FPS
-		0,                                     // pas de noeud parent
-		100.0f,                                // vitesse de rotation
-		0.1f,                                  // vitesse de deplacement
-		-1,                                    // pas de numero d'ID
-		keyMap,                                // on change la keymap
-		5);                                    // avec une taille de 5
+	sceneManager->addCameraSceneNodeFPS(                // ajout de la camera FPS
+		0, 100.0f, 0.1f, -1, keyMap, 5);
 
 
 	/* RENDU */
 
-	irr::video::SColor color(                  // contient la couleur blanc
-		255,                                   // composante A alpha (transparence)
-		255,                                   // composante R rouge
-		255,                                   // composante G verte
-		255);                                  // composante B bleue
-
-	while (device->run()) {                    // la boucle de rendu
-		driver->beginScene(true, true, color); // demarre le rendu
-		sceneManager->drawAll ();              // calcule le rendu
-		driver->endScene ();                   // affiche le rendu
+	while (device->run()) {                             // la boucle de rendu
+		driver->beginScene(true, true,                    // demarre le rendu
+			irr::video::SColor(0,255,255,255));
+		sceneManager->drawAll ();                         // calcule le rendu
+		driver->endScene ();                              // affiche le rendu
 	}
 
-	device->drop ();                           // libere la memoire
+	device->drop ();                                    // libere la memoire
 	return 0;
 }
