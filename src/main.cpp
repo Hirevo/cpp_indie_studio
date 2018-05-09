a4e76157fa958b1773c43dd5de70372faa037609
Benjamin <benjamin.solca@epitech.eu>
2018-05-09 16:52:16 +0200

    fix media loading

--- src/main.cpp
+++ src/main.cpp
@@ -7,59 +7,55 @@
 
 #include <irrlicht.h>
 
-
 int main(void) {
 
-	irr::IrrlichtDevice* device = irr::createDevice(    // creation du device
-		irr::video::EDT_OPENGL,                           // API = OpenGL
-		irr::core::dimension2d<irr::u32>(640,480),        // taille fenetre 640x480p
-		32);                                              // 32 bits par pixel
-	irr::video::IVideoDriver* driver =                  // creation du driver video
-		device->getVideoDriver ();
-	irr::scene::ISceneManager* sceneManager =           // creation du scene manager
-		device->getSceneManager ();
-	device->getCursorControl ()-> setVisible (false);   // rend le curseur invisible
+	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_OPENGL,
+		irr::core::dimension2d<irr::u32>(640,480), 32);
+	irr::video::IVideoDriver* driver = device->getVideoDriver();
+	irr::scene::ISceneManager* sceneManager = device->getSceneManager();
 
 
 	/* MODELE */
 
-	irr::scene::IAnimatedMeshSceneNode *sydney =        // cree un scene node nomme sydney
+	irr::scene::IAnimatedMeshSceneNode* sphere =        // cree un scene node nomme sphere
+		sceneManager->addAnimatedMeshSceneNode (          // via le scene manager
+			sceneManager->getMesh ("/opt/irrlicht-1.8.4/media/earth.x"),              // en chargeant le mesh "earth.x"
+			0, -1,                                          // pas de parent, pas d'ID
+			irr::core::vector3df(0.0f, 0.0f, 25.0f),        // position de la sphere
+			irr::core::vector3df(0.0f, 0.0f, 0.0f),         // rotation
+			irr::core::vector3df(15.0f, 15.0f, 15.0f));     // echelle
+
+	irr::scene::IAnimatedMeshSceneNode* sydney =        // cree un scene node nomme sydney
 		sceneManager->addAnimatedMeshSceneNode (          // via le scene manager
 			sceneManager->getMesh ("sydney.md2"));          // en chargeant le mesh "sydney.md2"
 
-	sydney->setMD2Animation(irr::scene::EMAT_STAND);    // joue l'animation STAND en boucle
 
-	sydney->setMaterialFlag(                            // modifie un material flag
-		irr::video::EMF_FOG_ENABLE, true);                // active le brouillard
+	/* OVERRIDE MATERIAL */
+
+	driver->getOverrideMaterial().EnableFlags =         // indique que le flag EMF_WIREFRAME
+		irr::video::EMF_WIREFRAME;                        // va etre outrepasse
+	driver->getOverrideMaterial().Material.setFlag(     // active le flag EMF_WIREFRAME
+		irr::video::EMF_WIREFRAME, true);                 // de l'override material
+	driver->getOverrideMaterial().EnablePasses =        // indique le type de node affectes
+		irr::scene::ESNRP_SOLID;                          // par l'override material
 
 
 	/* CAMERA */
 
-	irr::SKeyMap keyMap[5];                             // re-assigne les commandes
-	keyMap[0].Action = irr::EKA_MOVE_FORWARD;           // avancer
-	keyMap[0].KeyCode = irr::KEY_KEY_W;                 // w
-	keyMap[1].Action = irr::EKA_MOVE_BACKWARD;          // reculer
-	keyMap[1].KeyCode = irr::KEY_KEY_S;                 // s
-	keyMap[2].Action = irr::EKA_STRAFE_LEFT;            // a gauche
-	keyMap[2].KeyCode = irr::KEY_KEY_A;                 // a
-	keyMap[3].Action = irr::EKA_STRAFE_RIGHT;           // a droite
-	keyMap[3].KeyCode = irr::KEY_KEY_D;                 // d
-	keyMap[4].Action = irr::EKA_JUMP_UP;                // saut
-	keyMap[4].KeyCode = irr::KEY_SPACE;                 // barre espace
-
-	sceneManager->addCameraSceneNodeFPS(                // ajout de la camera FPS
-		0, 100.0f, 0.1f, -1, keyMap, 5);
+	sceneManager->addCameraSceneNode(0,                 // cree une camera fixe
+		irr::core::vector3df(40.0f, 10.0f, 15.0f),        // position de la cam
+		irr::core::vector3df(0.0f, 9.0f, 15.0f));         // cible de la cam
 
 
 	/* RENDU */
 
-	while (device->run()) {                             // la boucle de rendu
-		driver->beginScene(true, true,                    // demarre le rendu
-			irr::video::SColor(0,255,255,255));
-		sceneManager->drawAll ();                         // calcule le rendu
-		driver->endScene ();                              // affiche le rendu
+	while (device->run()) {                        // la boucle de rendu
+		driver->beginScene(true, true,               // demarre le rendu
+			irr::video::SColor(0,255,255,255));        // couleur blanche
+		sceneManager->drawAll ();                    // calcule le rendu
+		driver->endScene ();                         // affiche le rendu
 	}
 
-	device->drop ();                                    // libere la memoire
+	device->drop ();                               // libere la memoire
 	return 0;
 }
