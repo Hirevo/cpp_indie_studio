5e1983e484758e556aa2307f92fa9502d6b84e75
Benjamin <s.benjamin@live.fr>
2018-05-18 17:06:45 +0200

    Clean cMakelist and add inlude in core
    Former-commit-id: 1e3bbc3dfd1ea21e0ea51fe1db0ccbbc87e6dafe

--- inc/classes/Core.hpp
+++ inc/classes/Core.hpp
@@ -8,13 +8,15 @@
 #ifndef BOMBERMAN_CORE_HPP
 #define BOMBERMAN_CORE_HPP
 
+#include <memory>
+#include <stack>
 #include "Scene.hpp"
 
 namespace Eo {
 	class Core {
 	public:
 	private:
-		std::stack<std::unique_ptr<Scene>> _stateMachine;
+		std::stack<std::unique_ptr<Eo::Scene>> _stateMachine;
 	};
 }
 
