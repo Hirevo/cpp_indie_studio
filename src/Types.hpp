bc1712332362cd978a89a9080786db453b772713
Hirevo <nipolom@live.fr>
2018-05-30 22:50:40 +0200

    Added type aliases within the Eo namespace
    Former-commit-id: ac5abec8c325f51cb64864f6aa2fbde97dd57ba9

---
+++ src/Types.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_indie_studio
+** File description:
+** types
+*/
+
+#pragma once
+
+#include <irrlicht.h>
+
+namespace Eo {
+	using vec3 = irr::core::vector3df;
+};
