e4d8190b5f15d2551f343c4ef2a8663bc37f7f0d
Hirevo <nipolom@live.fr>
2018-05-30 21:37:29 +0200

    AMovable's methods are now marked as final
    Former-commit-id: 228a61272436867c61161c42d2760e8ffb76230d

--- src/abstracts/AMovable.hpp
+++ src/abstracts/AMovable.hpp
@@ -15,19 +15,19 @@ namespace Eo {
 		AMovable(irr::core::vector3df pos = irr::core::vector3df(
 				0, 0, 0));
 		~AMovable() = default;
-		irr::core::vector3df getPosition() const override;
-		void setPosition(const irr::core::vector3df &pos) override;
-		void setPosition(float x, float y, float z) override;
-		void setPosition(float v) override;
-		void translate(const irr::core::vector3df &v) override;
-		void translate(float v) override;
-		void translate(float x, float y, float z) override;
-		void translateX(float x) override;
-		void translateY(float y) override;
-		void translateZ(float z) override;
-		bool hasPositionChanged() const override;
-		void setPositionChanged(bool changed) override;
-		void resetPositionChanged() override;
+		irr::core::vector3df getPosition() const final;
+		void setPosition(const irr::core::vector3df &pos) final;
+		void setPosition(float x, float y, float z) final;
+		void setPosition(float v) final;
+		void translate(const irr::core::vector3df &v) final;
+		void translate(float v) final;
+		void translate(float x, float y, float z) final;
+		void translateX(float x) final;
+		void translateY(float y) final;
+		void translateZ(float z) final;
+		bool hasPositionChanged() const final;
+		void setPositionChanged(bool changed) final;
+		void resetPositionChanged() final;
 
 	protected:
 		irr::core::vector3df _pos;
