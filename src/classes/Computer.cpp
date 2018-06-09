cc1e870d4d8b0a42643c6cb502e01b3b2f7abeac
Romain <romainlaemmel@gmail.com>
2018-06-09 14:02:35 +0200

    IA v1.0 => Random move, pose bombs near dest wall
    Former-commit-id: cc29bd4ba1af6b405b59de7e24b19cbcdb77e477

--- src/classes/Computer.cpp
+++ src/classes/Computer.cpp
@@ -13,4 +13,90 @@ Eo::Computer::Computer(Eo::Rc<Eo::IScene> game, const Eo::vec3 &pos)
 	this->loadModel(game, "../assets/model-test/sydney.md2",
 		"../assets/model-test/sydney.bmp");
 	this->getAnimatedNode()->setScale(Eo::vec3(0.020f));
+	_counter = 0;
+	_speed = 0.03f;
+	_dir = Eo::vec3(0);
+}
+
+Eo::f32 Eo::Computer::getSpeed() const
+{
+	return _speed;
+}
+
+Eo::vec3 Eo::Computer::getDirection() const
+{
+	return _dir;
+}
+
+Eo::i32 Eo::Computer::getCounter() const
+{
+	return _counter;
+}
+
+void Eo::Computer::searchDirection(Eo::Rc<Eo::Map> map)
+{
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::vector<std::pair<Eo::vec3, Eo::i32>> directions;
+
+	directions.push_back(std::make_pair(vec3(0, 0, _speed), 270));
+	directions.push_back(std::make_pair(vec3(0, 0, -_speed), 90));
+	directions.push_back(std::make_pair(vec3(_speed, 0, 0), 0));
+	directions.push_back(std::make_pair(vec3(-_speed, 0, 0), 180));
+	std::uniform_real_distribution<> dist(0, directions.size());
+	Eo::i32 rand = dist(mt);
+	_dir = directions.at(rand).first;
+	this->setRotation(directions.at(rand).second);
+	dist = std::uniform_real_distribution<>(10, 150);
+	_counter = dist(mt);
+}
+
+#include <iostream>
+
+void Eo::Computer::updatePosition(Eo::Rc<Eo::Map> map)
+{
+	if (_counter == 0)
+		searchDirection(map);
+	if (isValidDirection(this->getPosition() + _dir, map)) {
+		this->translate(_dir);
+		_counter--;
+	} else
+		_counter = 0;
+}
+
+bool Eo::Computer::checkPoseBomb(Eo::Rc<Eo::Map> map)
+{
+	bool ret = false;
+	std::vector<Eo::IObject::Type> outline;
+	auto type = getObjectType(this->getPosition(), map);
+
+	if (type != IObject::BOMB) {
+		outline.push_back(getObjectType(vec3(this->getPosition() + vec3(0, 0, 0.5)), map));
+		outline.push_back(getObjectType(vec3(this->getPosition() + vec3(0, 0, -0.5)), map));
+		outline.push_back(getObjectType(vec3(this->getPosition() + vec3(0.5, 0, 0)), map));
+		outline.push_back(getObjectType(vec3(this->getPosition() + vec3(-0.5, 0, 0)), map));
+		if (std::find(outline.begin(), outline.end(), Eo::IObject::DEST_WALL) != outline.end()) {
+			ret = true;
+		}
+	}
+	return ret;
+}
+
+Eo::IObject::Type Eo::Computer::getObjectType(Eo::vec3 pos, Eo::Rc<Eo::Map> map)
+{
+	IObject::Type type;
+
+	auto posX = roundf(pos.X) + map->getWidth() / 2;
+	auto posY = roundf(pos.Z) + map->getHeight() / 2;
+	auto object = map->getObject(posX, posY);
+	if (object)
+		type = object->getType();
+	return type;
+}
+
+bool Eo::Computer::isValidDirection(Eo::vec3 pos, Eo::Rc<Eo::Map> map)
+{
+	auto type = getObjectType(pos, map);
+
+	return !(type == IObject::WALL || type == IObject::DEST_WALL);
 }
--- src/classes/Computer.hpp
+++ src/classes/Computer.hpp
@@ -8,11 +8,26 @@
 #pragma once
 
 #include "AModel.hpp"
+#include "Map.hpp"
+#include <vector>
+#include <utility>
+#include <random>
 
 namespace Eo {
 	class Computer : public AModel {
 	public:
 		Computer(Eo::Rc<Eo::IScene> game, const Eo::vec3 &pos);
-	private:
+		Eo::f32 getSpeed() const;
+		Eo::vec3 getDirection() const;
+		Eo::i32 getCounter() const;
+		void searchDirection(Eo::Rc<Eo::Map> map);
+		void updatePosition(Eo::Rc<Eo::Map> map);
+		bool checkPoseBomb(Eo::Rc<Eo::Map> map);
+		Eo::IObject::Type getObjectType(Eo::vec3 pos, Eo::Rc<Eo::Map> map);
+		bool isValidDirection(Eo::vec3 pos, Eo::Rc<Eo::Map> map);
+	public:
+		Eo::f32 _speed;
+		Eo::vec3 _dir;
+		Eo::i32 _counter;
 	};
 }
