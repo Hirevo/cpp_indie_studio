0b9f8df6ecbf68ea950e179a6e08ef9b82add447
maxime <maxime.jenny@epitech.eu>
2018-06-09 16:45:45 +0200

    push all the colors of the bomberman.
    Signed-off-by: Maxime JENNY <maxime.jenny@epitech.eu>
    
    Former-commit-id: f8356f619cf38d672ddfeb72066a0d01209b597f

--- src/classes/Computer.cpp
+++ src/classes/Computer.cpp
@@ -11,9 +11,15 @@ Eo::Computer::Computer(Eo::Rc<Eo::IScene> game, const Eo::vec3 &pos,
 	Eo::u32 playerID)
 	: AModel(Eo::IObject::Type::CHARACTER, pos), ACharacter(playerID)
 {
-	this->loadModel(game, "../assets/model-test/sydney.md2",
-		"../assets/model-test/sydney.bmp");
-	this->getAnimatedNode()->setScale(Eo::vec3(0.020f));
+	std::stringstream path;
+	path << "../assets/img/bomberboy_";
+	path << playerID + 1;
+	path << ".png";
+	this->loadModel(game, "../assets/Bomberman/Character.x",
+		path.str().c_str());
+	_scale = vec3(0.28f);
+	this->getAnimatedNode()->setScale(_scale);
+	this->getAnimatedNode()->setAnimationSpeed(2000.f);
 	_counter = 0;
 	_dir = Eo::vec3(0);
 }
